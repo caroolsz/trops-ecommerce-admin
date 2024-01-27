75dcd55fba3d63937a9e1a60cf85981b78eb7adc
devcarol03 <devcarol03@gmail.com>
2024-01-27 23:08:25 +0000

    Dashboard Page (Graphs) & Dark Mode

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,11 +1,14 @@
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
 import { getSalesCount } from "@/actions/get-sales.count";
+import { getStockCount } from "@/actions/get-stock-count";
 import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { Overview } from "@/components/overview";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
 import { formatter } from "@/lib/utils";
-import { CreditCardIcon, EuroIcon, Package } from "lucide-react";
+import { CreditCard, CreditCardIcon, EuroIcon, Package } from "lucide-react";
 
 interface DashBoardPageProps {
     params: { storeId: string }
@@ -16,12 +19,13 @@ const DashBoardPage: React.FC<DashBoardPageProps> = async({
 }) => {
     const totalRevenue = await getTotalRevenue(params.storeId);
     const salesCount = await getSalesCount(params.storeId);
-    const stockCount = await getTotalRevenue(params.storeId);
+    const stockCount = await getStockCount(params.storeId);
+    const graphRevenue = await getGraphRevenue(params.storeId);
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <Heading title="Dashboard" description="Overview of your store"/>
+                <Heading title="Dashboard" description="Overview of your store" />
                 <Separator />
                 <div className="grid gap-4 grid-cols-3">
                     <Card>
@@ -29,7 +33,7 @@ const DashBoardPage: React.FC<DashBoardPageProps> = async({
                             <CardTitle className="text-sm font-medium">
                                 Total Revenue
                             </CardTitle>
-                            <EuroIcon className="h-4 w-4 text-muted-foreground"/>
+                            <EuroIcon className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
                         <CardContent>
                             <div className="text-2xl font-bold">
@@ -37,13 +41,12 @@ const DashBoardPage: React.FC<DashBoardPageProps> = async({
                             </div>
                         </CardContent>
                     </Card>
-
                     <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                             <CardTitle className="text-sm font-medium">
                                 Sales
                             </CardTitle>
-                            <CreditCardIcon className="h-4 w-4 text-muted-foreground"/>
+                            <CreditCard className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
                         <CardContent>
                             <div className="text-2xl font-bold">
@@ -51,25 +54,33 @@ const DashBoardPage: React.FC<DashBoardPageProps> = async({
                             </div>
                         </CardContent>
                     </Card>
-
                     <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                             <CardTitle className="text-sm font-medium">
-                                Products in Stock
+                                Products In Stock
                             </CardTitle>
-                            <Package className="h-4 w-4 text-muted-foreground"/>
+                            <Package className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
                         <CardContent>
                             <div className="text-2xl font-bold">
-                                12
+                                {stockCount}
                             </div>
                         </CardContent>
                     </Card>
-
                 </div>
+                <Card className="col-span-4">
+                    <CardHeader>
+                        <CardTitle>
+                            Overview
+                        </CardTitle>
+                        <CardContent className="pl-2">
+                            <Overview data={graphRevenue} />
+                        </CardContent>
+                    </CardHeader>
+                </Card>
             </div>
         </div>
     );
-}
+};
 
 export default DashBoardPage;
